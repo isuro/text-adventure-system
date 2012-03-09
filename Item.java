79fded9ef78e49fcb6eed8de1851d80fce741860
Alison Mayer <alisonhmayer@gmail.com>
2012-03-09 16:17:45 -0500

    Added ItemBag, modified Item.

--- Item.java
+++ Item.java
@@ -3,10 +3,88 @@ public class Item
 	String name;
 	String shortName;
 	String description;
+	String shortDescription;
 	String useEffect;
-	int weight;
-	boolean inInventory;
+	Item [] usedWith;
+	int id;
 
-	
+	public Item (String itemName, String itemShortName, String itemDescription,
+	String itemShortDescription, String itemUseEffect, Item [] itemUsedWith,
+	int itemID)
+	{
+		this.name = itemName;
+		this.shortName = itemShortName;
+		this.description =  itemDescription;
+		this.shortDescription = itemShortDescription;
+		this.useEffect = itemUseEffect;
+		this.usedWith = itemUsedWith;
+		this.id = itemID;
+	}
 
+	public Item getItem (Item i)
+	{
+		return i;
+	}
+
+	public String getName ()
+	{
+		return name;
+	}
+
+	public String getShortName ()
+	{
+		return shortName;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public String getShortDescription()
+	{
+		return shortDescription;
+	}
+
+	public String getUseEffect()
+	{
+		return useEffect;
+	}
+
+	public int getID()
+	{
+		return id;
+	}
+
+	public void setName (String n)
+	{
+		name = n;
+	}
+
+	public void setShortName (String sN)
+	{
+		shortName = sN;
+	}
+
+	public void setDescription (String d)
+	{
+		description = d;
+	}
+
+	public void setShortDescription (String sD)
+	{
+		shortDescription = sD;
+	}
+
+	public void setUseEffect (String u)
+	{
+		useEffect = u;
+	}
+
+	public void setInInventory (int i)
+	{
+		id = i;
+	}
+
+}
 
