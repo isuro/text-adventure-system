e0746eee4b6e8b2ffd6653dc8efd66abad84b7bd
Alison Mayer <alisonhmayer@gmail.com>
2012-03-22 11:46:38 -0400

    Added increaseCapacity method, cleaned up class.

--- ItemBag.java
+++ ItemBag.java
@@ -2,19 +2,21 @@ import java.lang.*;
 
 public class ItemBag
 {
-	public Item [] items;
-	public int numberOfItems;
+	private Item [] items;
+	private int numberOfItems;
+	private final int INCREMENT;
 
 	public ItemBag()
 	{
 		items = new Item [0];
 		numberOfItems = 0;
+		INCREMENT = 10;
 	}
 
 	public void addItem(Item i)
 	{
-		if (items.isFull)
-			items.increaseCapacity(1);
+		if (items.isFull())
+			items.increaseCapacity(INCREMENT);
 		else
 		{
 			i = items[numberOfItems];
@@ -24,14 +26,18 @@ public class ItemBag
 
 	public boolean isFull()
 	{
-		if ((items.length() - 1) == numberOfItems)
+		if ((items.length - 1) == numberOfItems)
 			return true;
 		else
 			return false;	
 	}
 
-	public void increaseCapacity()
+	public void increaseCapacity(int newSize)
 	{
-		
+		Item [] old = items;
+        items = (Item []) new Comparable[ newSize ];
+        for(int i = 0; i < old.length; i++ )
+            items[ i ] = old[ i ]; 
 	}
+
 }
