e8b9c47450152c182179c0a3dd9b7b37782dbdf4
Isaac Zarsky <idudez@gmail.com>
2012-04-16 15:37:30 -0400

    More vectors! No charge!

--- RoomBag.java
+++ RoomBag.java
@@ -5,73 +5,49 @@
 * @date April 4, 2012
 * @version 1.1
 */
+import java.util.Vector;
 
 public class RoomBag
 {
-	public Room[] rooms;
-	int numberOfRooms;
-	final int INCREMENT = 10;
+	public Vector<Room> rooms;
 
 	public RoomBag()
 	{
-		rooms = new Room[0];
-		numberOfRooms = 0;
+		rooms = new Vector<Room>();
 	}
 
 	public void addRoom(Room r)
 	{
-		if (isFull())
-			increaseCapacity(INCREMENT);
-		else
-		{
-			r = rooms[numberOfRooms];
-			numberOfRooms++;	
-		}
-	}
-
-	public boolean isFull()
-	{
-		if ((rooms.length - 1) == numberOfRooms)
-			return true;
-		else
-			return false;	
-	}
-
-	public void increaseCapacity(int newSize)
-	{
-		Room [] old = rooms;
-        rooms = (Room []) new Comparable[ newSize ];
-        for(int i = 0; i < old.length; i++ )
-            rooms[ i ] = old[ i ]; 
+		rooms.add(r);
 	}
 
 	//returns array of rooms
-	public Room[] getRooms()
-	{
-		return rooms; 
-	}
+	//public Room[] getRooms()
+	//{
+	//	return rooms; 
+	//}
 
 	//returns room with user-given index from the array of rooms
 	public Room getARoom(int i)
 	{
-		return rooms[i];
+		return rooms.elementAt(i);
 	}
 
 	public int getNumberOfRooms()
 	{
-		return numberOfRooms;
+		return rooms.size();
 	}
 
 	//prints items
 	public void printRooms()
     { 
-        if (numberOfRooms == 0)
+        if (rooms.size() == 0)
             System.out.println("There are no rooms.");
         else
         {
-            System.out.println("\n There are currently "+ numberOfRooms +" rooms:");
-            for (int i = 0; i < numberOfRooms; i++)
-                System.out.print(rooms[i]+ " ");
+            System.out.println("\n There are currently "+ rooms.size() +" rooms:");
+            for (int i = 0; i < rooms.size(); i++)
+                System.out.print(rooms.elementAt(i)+ " ");
             System.out.println();
         }                
     }
